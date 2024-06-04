b20d9bddd53fd9a109a560fec85fb76ccd51b6f5
vijaykumarme <vijaykdev19@gmail.com>
2024-06-04 10:06:10 +0530

    Modified api url

--- server/db.js
+++ server/db.js
@@ -1,20 +1,8 @@
-// const {Pool} = require("pg");
+const {Pool} = require("pg");
 
-// const pool = new Pool();
-// module.exports = {
-//   query: (text,params) => pool.query(text,params)
-// }
-
-// With credentials
-
-const Pool = require("pg").Pool;
-
-const pool = new Pool({
-    user: 'postgres',
-    password: 'Kumar@369*',
-    host: '172.232.123.22',
-    database: 'myexpenses',
-    port: 5432
-});
+const pool = new Pool();
+module.exports = {
+  query: (text,params) => pool.query(text,params)
+}
 
 module.exports = pool;
