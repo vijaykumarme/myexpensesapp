7b7b59dfe6111c8d0051e53664eb976f95d50ae2
vijaykumarme <vijaykdev19@gmail.com>
2024-06-13 21:48:43 +0530

    Update api url v5

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext, Fragment} from "react";
+import React, { useState, useEffect, useContext, Fragment } from "react";
 import axios from "axios";
 import { Pie, Line, Bar } from 'react-chartjs-2';
 import {
@@ -11,15 +11,12 @@ import {
     Tooltip,
     Legend,
     ArcElement,
-} from 'chart.js'
-//import { Chart, ArcElement, Legend, Tooltip } from 'chart.js';
+} from 'chart.js';
 import moment from 'moment';
 
 //components
 import AppContext from "../AppContext";
 import api from "../api/ApiURL";
-
-
 
 ChartJS.register(
     CategoryScale, 
@@ -47,91 +44,74 @@ const MonthNames = [
     { monthId: 12, monthName: 'Dec' }
 ];
 
-
 const Home = () => {
-
-    const {setUsername} = useContext(AppContext)
-    const {userName} = useContext(AppContext)
-    const {setUseremail} = useContext(AppContext)
-    const {useremail} = useContext(AppContext)
+    const { setUsername } = useContext(AppContext);
+    const { userName } = useContext(AppContext);
+    const { setUseremail } = useContext(AppContext);
+    const { useremail } = useContext(AppContext);
 
     const [getCurrentMonthExpenses, setGetCurrentMonthExpenes] = useState([]);
-    const [getAllExpenses, setGetAllExpenses] = useState([])
-    const [getCurrentMonthIncome, setGetCurrentMonthIncome] = useState([])
-
+    const [getAllExpenses, setGetAllExpenses] = useState([]);
+    const [getCurrentMonthIncome, setGetCurrentMonthIncome] = useState([]);
 
     const dateObject = new Date();
-    const currentMonth = dateObject.getMonth()+1;
+    const currentMonth = dateObject.getMonth() + 1;
     const currentYear = dateObject.getFullYear();
-    const currentMonthName = MonthNames.find(month => month.monthId === currentMonth)
+    const currentMonthName = MonthNames.find(month => month.monthId === currentMonth);
 
-
-    async function getCurrentMonthExpensesFunc () {
-        try {
-            const responsemonthly = await api.post("/api/getcurrentmonthexpenses",{currentMonth,currentYear,useremail});
-            console.log(responsemonthly)
-            console.log(responsemonthly.data)
-            setGetCurrentMonthExpenes(responsemonthly.data)
-            const responseall = await api.post("/api/getmonthexpenses",{useremail});
-            setGetAllExpenses(responseall.data)
-            const getcurrentincome = await api.post("/api/getmonthlyincome",{currentYear,currentMonth,useremail});
-            setGetCurrentMonthIncome(getcurrentincome.data)
-        } catch(err) {
-            console.log(err.message)
-        }
-    }
-
-    async function getName() {
-        try {
-            // const response = await fetch("http://mynewexpenses.xyz/api/userdashboard/",{
-            //     method: "GET",
-            //     headers: {token: localStorage.token}
-            // })
-
-            const response = await api.get('/api/userdashboard/', {
-                headers: { token: localStorage.token }
-            });
-
-
-            const parseRes = response.data
-            const currentUsername = parseRes.username
-            const currentUserEmail = parseRes.useremail
-            setUsername(currentUsername)
-            setUseremail(currentUserEmail)
-            
-        } catch(err) {
-            console.error(err.message);
-        }
-    }
-
+    // Fetching user data once when the component mounts
     useEffect(() => {
-        getName();
-    },[])
+        const fetchUserData = async () => {
+            try {
+                const response = await api.get('/api/userdashboard/', {
+                    headers: { token: localStorage.token }
+                });
+                const { username, useremail } = response.data;
+                setUsername(username);
+                setUseremail(useremail);
+            } catch (err) {
+                console.error(err.message);
+            }
+        };
 
+        fetchUserData();
+    }, []); // Empty dependency array, runs only once
+
+    // Fetching expenses and income whenever useremail changes
     useEffect(() => {
-        getCurrentMonthExpensesFunc();
-    },[userName])
+        const fetchFinancialData = async () => {
+            try {
+                if (useremail) {
+                    const responseMonthly = await api.post("/api/getcurrentmonthexpenses", { currentMonth, currentYear, useremail });
+                    setGetCurrentMonthExpenes(responseMonthly.data);
+                    const responseAll = await api.post("/api/getmonthexpenses", { useremail });
+                    setGetAllExpenses(responseAll.data);
+                    const getCurrentIncome = await api.post("/api/getmonthlyincome", { currentYear, currentMonth, useremail });
+                    setGetCurrentMonthIncome(getCurrentIncome.data);
+                }
+            } catch (err) {
+                console.log(err.message);
+            }
+        };
 
+        fetchFinancialData();
+    }, [useremail]); // Depends on useremail
+
+    // Calculate gradient color
     const calculateGradientColor = (amounts, maxAmount, minAmount) => {
         const ratio = (amounts - minAmount) / (maxAmount - minAmount);
         const r = Math.floor(255 * ratio); // Red component increases with ratio
         const g = Math.floor(255 * (1 - ratio)); // Green component decreases with ratio
         const b = 0; // Blue component is kept at 0 to maintain the red-green gradient
-        return `rgba(${r}, ${g}, ${b}, 0.9)`; // Alpha value is 0.6 for semi-transparency
+        return `rgba(${r}, ${g}, ${b}, 0.9)`; // Alpha value is 0.9 for semi-transparency
     };
-    
-
 
     const categories = getCurrentMonthExpenses.map(item => item.categoryname);
     const amounts = getCurrentMonthExpenses.map(item => item.totalamount);
-
-    const maxAmount = Math.max(...amounts)
-    const minAmount = Math.min(...amounts)
-    const TotalMonthAmount = amounts.reduce((total,amount) => total+parseFloat(amount),0).toFixed(2);
-
-    const backgroundColors = amounts.map(amount => 
-        calculateGradientColor(amount,maxAmount,minAmount)
-    );
+    const maxAmount = Math.max(...amounts);
+    const minAmount = Math.min(...amounts);
+    const TotalMonthAmount = amounts.reduce((total, amount) => total + parseFloat(amount), 0).toFixed(2);
+    const backgroundColors = amounts.map(amount => calculateGradientColor(amount, maxAmount, minAmount));
 
     const pieChartData = {
         labels: categories,
@@ -143,11 +123,9 @@ const Home = () => {
                 hoverOffset: 4,
             }
         ]
-    }
+    };
 
-    // LineChart ----------------------------------
-
-
+    // Organize data for line chart
     const organizedData = {};
     getAllExpenses.forEach(expense => {
         const { date, month, year, categoryname, amount } = expense;
@@ -162,14 +140,11 @@ const Home = () => {
         }
     });
 
-
-    //Function to generate line chart data
-
     const categorynames = Object.values(organizedData)
         .flatMap(entry => Object.keys(entry))
         .filter((value, index, self) => self.indexOf(value) === index);
 
-    const labels = Object.keys(organizedData)
+    const labels = Object.keys(organizedData);
 
     const getRandomColor = () => {
         // Generate random RGB values
@@ -182,37 +157,35 @@ const Home = () => {
 
     let datasets = [];
 
-    // Iterate over meal types
+    // Iterate over category names
     categorynames.forEach(type => {
-      let data = [];
+        let data = [];
 
-      // Iterate over dates
-      Object.keys(organizedData).forEach(date => {
-        // Check if the meal type exists for the current date
-        const value = organizedData[date][type] ? parseFloat(organizedData[date][type]) : 0;
-        data.push(value);
-      });
+        // Iterate over dates
+        Object.keys(organizedData).forEach(date => {
+            // Check if the category type exists for the current date
+            const value = organizedData[date][type] ? parseFloat(organizedData[date][type]) : 0;
+            data.push(value);
+        });
 
-      // Push dataset object
-      datasets.push({
-        label: type,
-        data: data,
-        borderColor: getRandomColor() // Assigning different border color for Breakfast and Lunch
-      });
+        // Push dataset object
+        datasets.push({
+            label: type,
+            data: data,
+            borderColor: getRandomColor() // Assigning different border color
+        });
     });
 
-    const generateLineChartData = {};
-    generateLineChartData.labels = labels;
-    generateLineChartData.datasets = datasets.map((dataset, index) => ({
-        label: dataset.label,
-        data: dataset.data,
-        borderColor: dataset.borderColor,
-    }));
+    const generateLineChartData = {
+        labels: labels,
+        datasets: datasets.map((dataset, index) => ({
+            label: dataset.label,
+            data: dataset.data,
+            borderColor: dataset.borderColor,
+        }))
+    };
 
-    //const options = {};
-
-    // Bar Chart
-
+    // Bar Chart data
     const dailyExpenses = {};
     getAllExpenses.forEach(expense => {
         const { date, amount } = expense;
@@ -224,13 +197,9 @@ const Home = () => {
 
     const days = Object.keys(dailyExpenses).sort((a, b) => a - b);
     const dailyAmounts = days.map(day => dailyExpenses[day]);
-
     const maxDailyAmount = Math.max(...dailyAmounts);
     const minDailyAmount = Math.min(...dailyAmounts);
-
-    const barChartColors = dailyAmounts.map(amount => 
-        calculateGradientColor(amount, maxDailyAmount, minDailyAmount)
-    );
+    const barChartColors = dailyAmounts.map(amount => calculateGradientColor(amount, maxDailyAmount, minDailyAmount));
 
     const barChartData = {
         labels: days,
@@ -245,67 +214,61 @@ const Home = () => {
         ]
     };
 
-    // const [getCurrentMonthExpenses, setGetCurrentMonthExpenes] = useState([]);
-    // const [getAllExpenses, setGetAllExpenses] = useState([])
-    // const [getCurrentMonthIncome, setGetCurrentMonthIncome] = useState([])
-
-    console.log(getCurrentMonthExpenses)
+    console.log("Current Month Expenses:", getCurrentMonthExpenses);
 
     return (
         <Fragment>
-        <div className="container">
-        {/* <div className="pt-2 container"> */}
-            <div className="card my-2">
-                <div className="card-header bg-primary text-white">
-                    <h4 className="mb-0 text-center">Current Month Information</h4>
+            <div className="container">
+                <div className="card my-2">
+                    <div className="card-header bg-primary text-white">
+                        <h4 className="mb-0 text-center">Current Month Information</h4>
+                    </div>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-12 col-md-6 mb-3 mb-md-0">
+                                <p className="p mb-0">Month: <span className="text-primary">{currentMonthName.monthName}</span></p>
+                            </div>
+                            <div className="col-12 col-md-6 text-md-right">
+                                {getCurrentMonthIncome.amount && <p className="p mb-2 mb-md-0">Month Income: &#x20B9;<span className="text-success">{getCurrentMonthIncome.amount}</span></p>}
+                                <p className="p mb-2 mb-md-0">Expenses: &#x20B9;<span className="text-danger">{TotalMonthAmount}</span></p>
+                                {getCurrentMonthIncome.amount && <p className="p mb-0">Saving: &#x20B9;<span className="text-success">{getCurrentMonthIncome.amount - TotalMonthAmount}</span></p>}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="card-body">
-                    <div className="row">
-                        <div className="col-12 col-md-6 mb-3 mb-md-0">
-                            <p className="p mb-0">Month: <span className="text-primary">{currentMonthName.monthName}</span></p>
+                <div className="card my-2">
+                    <div className="card-header bg-primary text-white">
+                        <h4 className="mb-0 text-center">Charts</h4>
+                    </div>
+                    <div id="carouselExampleControls" className="carousel slide py-2" data-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <div>
+                                    <Pie data={pieChartData} className="d-block h-100 w-100" alt="First slide" />
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <div className="parent-container pt-4" style={{ height: "500px" }}>
+                                    <Bar data={barChartData} className="d-block w-100" alt="Third slide" />
+                                </div>
+                            </div>
+                            {/* <div className="carousel-item">
+                                <div className="parent-container pt-4" style={{ height: "500px" }}>
+                                    <Line data={forecastLineChartData} className="d-block w-100" alt="Forecast slide" />
+                                </div>
+                            </div> */}
                         </div>
-                        <div className="col-12 col-md-6 text-md-right">
-                            {getCurrentMonthIncome.amount && <p className="p mb-2 mb-md-0">Month Income: &#x20B9;<span className="text-success">{getCurrentMonthIncome.amount}</span></p>}
-                            <p className="p mb-2 mb-md-0">Expenses: &#x20B9;<span className="text-danger">{TotalMonthAmount}</span></p>
-                            {getCurrentMonthIncome.amount && <p className="p mb-0">Saving: &#x20B9;<span className="text-success">{getCurrentMonthIncome.amount-TotalMonthAmount}</span></p>}
-                        </div>
+                        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Next</span>
+                        </a>
                     </div>
                 </div>
             </div>
-        {/* </div> */}
-        <div className="card my-2">
-        <div className="card-header bg-primary text-white">
-            <h4 className="mb-0 text-center">Charts</h4>
-        </div>
-        <div id="carouselExampleControls" className="carousel slide py-2" data-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <div>
-                    <Pie data={pieChartData} className="d-block h-100 w-100" alt="First slide" />
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="parent-container pt-4" style={{ height: "500px" }}>
-                        <Bar data={barChartData} className="d-block w-100" alt="Third slide" />
-                    </div>
-                </div>
-                {/* <div className="carousel-item">
-                    <div className="parent-container pt-4" style={{ height: "500px" }}>
-                        <Line data={forecastLineChartData} className="d-block w-100" alt="Forecast slide" />
-                    </div>
-                </div> */}
-            </div>
-            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-            </a>
-        </div>
-        </div>
-        </div>
         </Fragment>
     );
 }
--- server/server.js
+++ server/server.js
@@ -240,6 +240,9 @@ app.delete("/api/deletemonthincome/:id", async(req,res) => {
 //Server Port
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+app.listen(5000, () => {
+  console.log("Server is running on 5000")
+})
